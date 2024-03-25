feb51e6d657fd1901b5fe8841b775d6a8de3ca86
HarryD1447 <harry@lecturegenie.co.uk>
2024-03-25 22:36:30 +0000

    Removed initial elements

--- Robotics-uk-interview/src/Pages/ScheduleEditorPage/ScheduleEditorPage.tsx
+++ Robotics-uk-interview/src/Pages/ScheduleEditorPage/ScheduleEditorPage.tsx
@@ -102,27 +102,27 @@ const DEFAULT_SCHEDULES: ISchedule[] = [
   },
 ];
 
-const initialNodes: Node[] = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-];
-const initialEdges: Edge[] = [
-  {
-    id: "e1-2",
-    source: "1",
-    target: "2",
-    type: "smoothstep",
-    pathOptions: {
-      borderRadius: 5,
-    },
-    markerEnd: {
-      type: MarkerType.ArrowClosed,
-      width: 18,
-      height: 18,
-      color: "#B6B6BC",
-    },
-  },
-];
+// const initialNodes: Node[] = [
+//   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+//   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+// ];
+// const initialEdges: Edge[] = [
+//   {
+//     id: "e1-2",
+//     source: "1",
+//     target: "2",
+//     type: "smoothstep",
+//     pathOptions: {
+//       borderRadius: 5,
+//     },
+//     markerEnd: {
+//       type: MarkerType.ArrowClosed,
+//       width: 18,
+//       height: 18,
+//       color: "#B6B6BC",
+//     },
+//   },
+// ];
 
 enum ActiveTab {
   Schedules,
@@ -144,8 +144,8 @@ const ScheduleEditorPage = () => {
   const [containerPosition, setContainerPosition] = React.useState({ x: 0, y: 0 });
   const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance | null>(null);
 
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
   const nodeTypes = {
     "type-transfer": TransferFlowCard,
---
+++ Robotics-uk-interview/src/Pages/ScheduleEditorPage/defaultData.ts
@@ -0,0 +1,21 @@
+// const initialNodes: Node[] = [
+//   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+//   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+// ];
+// const initialEdges: Edge[] = [
+//   {
+//     id: "e1-2",
+//     source: "1",
+//     target: "2",
+//     type: "smoothstep",
+//     pathOptions: {
+//       borderRadius: 5,
+//     },
+//     markerEnd: {
+//       type: MarkerType.ArrowClosed,
+//       width: 18,
+//       height: 18,
+//       color: "#B6B6BC",
+//     },
+//   },
+// ];

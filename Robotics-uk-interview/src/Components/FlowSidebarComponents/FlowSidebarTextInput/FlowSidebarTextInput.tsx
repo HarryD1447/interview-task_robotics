0f4cd88bdc63da99596c422d458bb452edb16842
HarryD1447 <harry@lecturegenie.co.uk>
2024-03-26 20:23:45 +0000

    Added copy button to textbox

--- Robotics-uk-interview/src/Components/FlowSidebarComponents/FlowSidebarTextInput/FlowSidebarTextInput.tsx
+++ Robotics-uk-interview/src/Components/FlowSidebarComponents/FlowSidebarTextInput/FlowSidebarTextInput.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FlowSidebarTextInput.scss";
 import ComponentWithLabel from "../ComponentWithLabel/ComponentWithLabel";
+import { MdCopyAll } from "react-icons/md";
+import { Tooltip, useToast } from "@chakra-ui/react";
 
 interface IFlowSidebarTextInputProps {
   value: string;
@@ -9,15 +11,74 @@ interface IFlowSidebarTextInputProps {
   tooltip?: string;
 }
 
+const HOVER_COPY_TIMEOUT = 200;
+
 const FlowSidebarTextInput = ({ value, setValue, label, tooltip }: IFlowSidebarTextInputProps) => {
+  //Notification for when the value is copied to the clipboard
+  const toast = useToast();
+
+  //Handle the styling of the copy icon when hovering over it
+  const [isHovering, setIsHovering] = useState(false);
+  const [hoverTimer, setHoverTimer] = useState<number | null>(null);
+
+  const handleMouseEnter = () => {
+    const timer = setTimeout(() => setIsHovering(true), HOVER_COPY_TIMEOUT);
+    setHoverTimer(timer);
+  };
+
+  const handleMouseLeave = () => {
+    if (hoverTimer) {
+      clearTimeout(hoverTimer);
+    }
+    setIsHovering(false);
+    setHoverTimer(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (hoverTimer) {
+        clearTimeout(hoverTimer);
+      }
+    };
+  }, [hoverTimer]);
+
+  //Copy the value to the clipboard
+  const copyToClipboard = () => {
+    if (value === "") return;
+
+    navigator.clipboard.writeText(value);
+
+    toast({
+      title: "Copied to clipboard",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
   return (
     <ComponentWithLabel label={label} tooltip={tooltip}>
-      <input
-        className="flow-sidebar-text-input__input"
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+      <div className="flow-sidebar-text-input__container">
+        <input
+          className="flow-sidebar-text-input__input"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Tooltip label="Copy" openDelay={350} hasArrow isDisabled={value === ""}>
+          <div
+            className={`flow-sidebar-text-input__copy-container ${
+              isHovering && value !== "" ? "flow-sidebar-text-input__copy-container--hover" : ""
+            }`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={copyToClipboard}
+          >
+            <MdCopyAll className="flow-sidebar-text-input__copy-icon" />
+          </div>
+        </Tooltip>
+      </div>
     </ComponentWithLabel>
   );
 };
